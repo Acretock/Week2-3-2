89e0306647de919ebfa15e3799c6373221f4a7ae
Acretock <georgoncharov@gmail.com>
2021-04-19 18:49:13 +0500

    v 1.0

--- Week2-3-2/Week2-3-2.cpp
+++ Week2-3-2/Week2-3-2.cpp
@@ -1,20 +1,75 @@
-// Week2-3-2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <map>
+#include <string>
 #include <iostream>
+#include <sstream>
+using namespace std;
+
+void Change_Capital(map<string,string>& map, string country, string capital) {
+	if (map.count(country) == 0) {
+		map.insert({ country,capital });
+		cout << "Introduce new country " << country << " with capital " << capital << endl;
+	}
+	else if (map.find(country)->second == capital) {
+		cout << "Country " << country << " hasn't change it's capital" << endl;
+	}
+	else {
+		cout << "Country " << country << " has change capital from " << map.find(country)->second << " to " << capital << endl;
+		map.find(country)->second = capital;
+	}
+}
+
+void Rename(map<string, string>& map, string oldCoutry, string newCountry) {
+	if ((map.count(oldCoutry) == 0) || (map.count(newCountry) != 0) || (oldCoutry == newCountry))
+		cout << "Incorrect rename, skipping" << endl;
+	else {
+		cout << "Country " << oldCoutry << " with capital " << map.find(oldCoutry)->second << " has been renamed to " << newCountry << endl;
+		map.insert({ newCountry, map.find(oldCoutry)->second });
+		map.erase(map.find(oldCoutry));
+	}
+}
+
+void About(map<string, string>& map, string country) {
+	if (map.count(country) != 0)
+		cout << "Country " << country << " has capital " << map.find(country)->second << endl;
+	else
+		cout << "Country " << country << " don't exist" << endl;
+}
+
+void Dump(map<string, string>& map) {
+	if (map.empty())
+		cout << "There are no countries in the world" << endl;
+	else
+		for (auto& i : map)
+			cout << i.first << '/' << i.second << ' ';
+	cout << endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int N;
+	map<string,string> countries;
+	string line, str1, str2, command;
+	cin >> N;
+	for (int i = 0; i <= N; i++) {
+		getline(cin, line);
+		stringstream ss(line);
+		ss >> command;
+		if(ss.good())
+			ss >> str1;
+		if(ss.good())
+			ss >> str2;
+		if (command == "ABOUT") { 
+			About(countries, str1);
+		}
+		if (command == "RENAME") { 
+			Rename(countries, str1, str2);
+		}
+		if (command == "DUMP") {
+			Dump(countries);
+		}
+		if (command == "CHANGE_CAPITAL") {
+			Change_Capital(countries, str1, str2);
+		}
+	}
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
